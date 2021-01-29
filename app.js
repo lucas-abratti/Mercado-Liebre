431586656e60b2a742f1997f90ce002fccbbd42b
lucas-abratti <lucasabratti99@gmail.com>
2021-01-29 20:16:51 -0300

    adaptacion del app.listen para Heroku

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/login.html"))
 })
 
-app.listen(3000, ()=> {
+app.listen(process.env.PORT || 3000, ()=> {
     console.log('Server is Live 3002');
     console.log(path.resolve(__dirname, "./public"));
 })
